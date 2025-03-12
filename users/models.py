408ad4cb03f71557038df4d332a7baf0533a0bf9
hamrayev7 <samandarhamrayev555@gmail.com>
2025-03-12 12:22:31 +0500

    app and models created

---
+++ users/models.py
@@ -0,0 +1,60 @@
+import os.path
+
+from django.contrib.auth.models import BaseUserManager, AbstractBaseUser, PermissionsMixin
+from django.db import models
+
+
+def user_avatar_file_path(instance, filename):
+    ext = str(filename).split(".")[-1]
+    filename = f"user_{instance.id}.{ext}"
+    return os.path.join("user-avatars/", filename)
+
+
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra):
+        if not email:
+            raise ValueError('The Email Field is must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra):
+        extra.setdefault('is_staff', True)
+        extra.setdefault('is_superuser', True)
+        extra.setdefault('is_active', True)
+
+        if extra.get('is_staff') is not True:
+            raise ValueError("Superuser must have is_staff=True")
+        elif extra.get('is_superuser') is not True:
+            raise ValueError("Superuser must have is_superuser=True")
+        return self.create_user(email, password, **extra)
+
+class User(AbstractBaseUser, PermissionsMixin):
+    first_name = models.CharField(max_length=128, blank=True)
+    last_name = models.CharField(max_length=128, blank=True)
+    email = models.EmailField(unique=True)
+    gender = models.CharField(max_length=10, choices=[
+        ('male', 'Male'),
+        ('female', 'Female'),
+    ], blank=True)
+    avatar = models.ImageField(upload_to=user_avatar_file_path, null=True, blank=True)
+    born_date = models.DateField(null=True, blank=True)
+    is_active = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name', 'last_name']
+
+    def __str__(self):
+        return self.email
+
+    @property
+    def full_name(self):
+        return f"{self.first_name} {self.last_name}".strip()
+
